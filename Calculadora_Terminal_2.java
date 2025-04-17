95d4099a61718dbd62c1c903c765528268554afe
Luiza Matos <contatoluizamatos@gmail.com>
2025-04-17 14:56:56 -0300

    Update Calculadora_Terminal_2.java

--- Calculadora_Terminal_2.java
+++ Calculadora_Terminal_2.java
@@ -89,6 +89,239 @@ public class Calculadora_Terminal_2 {
                     System.out.println(" \n | PROGRAMA ENCERRADO! |\n");
                 }
                 break;
+
+                case 5: // porcentagem
+                System.out.printf("1. Adicionar porcentagem \n2. Subtrair porcentagem \n > O que deseja fazer? ");
+                int escolha = entrada.nextInt();
+                // adição de porcentagem
+                if (escolha == 1) {
+                    System.out.printf(" - Digite o número que será somado a porcentagem: ");
+                    double numero_porcentagem = entrada.nextDouble();
+                    System.out.println(" - Digite a porcentagem: ");
+                    double valor_porcentagem = entrada.nextDouble();
+                    double porcentagem = numero_porcentagem + ((numero_porcentagem * valor_porcentagem) / 100);
+                    System.out.println(" >> O resultado é: "+porcentagem);
+                    System.out.printf(" \n > Fim do programa, deseja voltar ao início? ");
+                    String voltar_5_1 = entrada.next();
+                    if (voltar_5_1.equals("sim")) {
+                        System.out.println(" \n > Voltando ao menu principal...\n");
+                        break;
+                    }
+                    else {
+                        voltarMenu = false;
+                        System.out.println(" \n | PROGRAMA ENCERRADO! |\n");
+                    }
+                    break;
+
+                } else {
+                    System.out.printf(" - Digite o número que será subtraído a porcentagem: ");
+                    double numero_porcentagem_subtrair = entrada.nextDouble();
+                    System.out.printf(" - digite a porcentagem: ");
+                    double valor_porcentagem_subtrair = entrada.nextDouble();
+                    double porcentagem_subtrair = numero_porcentagem_subtrair - ((numero_porcentagem_subtrair * valor_porcentagem_subtrair) / 100);
+                    System.out.println(" >> O resultado é: "+porcentagem_subtrair);
+                    System.out.printf(" \n > Fim do programa, deseja voltar ao início? ");
+                    String voltar_5_2 = entrada.next();
+                    if (voltar_5_2.equals("sim")) {
+                        System.out.println(" \n > Voltando ao menu principal...\n");
+                        break;
+                    }
+                    else {
+                        voltarMenu = false;
+                        System.out.println(" \n | PROGRAMA ENCERRADO! |\n");
+                    }
+                    break;
+                }
+
+                case 6: // potência
+                System.out.printf(" - Digite o número que deseja elevar (base): ");
+                double base = entrada.nextDouble();
+                System.out.printf(" - Digite o número que irá elevar (expoente): ");
+                double expoente = entrada.nextDouble();
+                double elevado = Math.pow(base, expoente);
+                System.out.println(" >> O resultado é: "+elevado);
+                System.out.println(" \n > Fim do programa, deseja voltar ao início? ");
+                String voltar_6 = entrada.next();
+                if (voltar_6.equals("sim")) {
+                    System.out.println(" \n > Voltando ao menu principal...\n");
+                    break;
+                }
+                else {
+                    voltarMenu = false;
+                    System.out.println(" \n | PROGRAMA ENCERRADO! |\n");
+                }
+                break;
+
+                case 7: // fração
+                System.out.println("1. Somar frações \n2. Subtrair fração \n3. Multiplicar frações \n4. Dividir frações");
+                System.out.printf(" > O que deseja fazer? ");
+                int escolha_7 = entrada.nextInt();
+                // adição de fração
+                if (escolha_7 == 1) {
+                    System.out.printf(" - Digite o numerador da 1º fração: ");
+                    int numerador_1 = entrada.nextInt();
+                    System.out.printf(" - Digite o denominador da 1º fração: ");
+                    int denominador_1 = entrada.nextInt();
+                    System.out.printf(" - Digite o numerador da 2º fração: ");
+                    int numerador_2 = entrada.nextInt();
+                    System.out.printf(" - Digite o denominador da 2º fração: ");
+                    int denominador_2 = entrada.nextInt();
+                    if (denominador_1 == denominador_2) {
+                        int soma_fracao = numerador_1 + numerador_2;
+                        System.out.println(" >> O resultado é: "+soma_fracao+" / "+ denominador_1);
+                        System.out.printf(" > Fim do programa, deseja voltar ao menu principal? ");
+                        String voltar_7_1_1 = entrada.next();
+                        if (voltar_7_1_1.equals("sim")) {
+                            System.out.println(" \n > Voltando ao menu principal...\n");
+                            break;
+                        }
+                        else {
+                            voltarMenu = false;
+                            System.out.println(" \n | PROGRAMA ENCERRADO! |\n");
+                        }
+                        break;
+                        // calculo de mmc, pois os denominadores são diferentes
+                    } else {
+                        int soma_fracao = numerador_1 + numerador_2;
+                        int maior, mmc;
+                        if (denominador_1 > denominador_2) {
+                            maior = denominador_1;
+                        } else {
+                            maior = denominador_2;
+                        }
+                        while(true) {
+                            if ((maior % denominador_1 == 0) && (maior % denominador_2 == 0)) {
+                                mmc = maior;
+                                break;
+                            }
+                            maior++;
+                        }
+                        System.out.println(" >> O resultado é: "+soma_fracao+" / "+mmc);
+                        System.out.printf(" > Fim do programa, deseja voltar ao menu principal? ");
+                        String voltar_7_1_2 = entrada.next();
+                        if (voltar_7_1_2.equals("sim")) {
+                            System.out.println(" \n > Voltando ao menu principal...\n");
+                            break;
+                        }
+                        else {
+                            voltarMenu = false;
+                            System.out.println(" \n | PROGRAMA ENCERRADO! |\n");
+                        }
+                    }
+                } 
+                if (escolha_7 == 2) {
+                    System.out.printf(" - Digite o numerador da 1º fração: ");
+                    int numerador_1 = entrada.nextInt();
+                    System.out.printf(" - Digite o denominador da 1º fração: ");
+                    int denominador_1 = entrada.nextInt();
+                    System.out.printf(" - Digite o numerador da 2º fração: ");
+                    int numerador_2 = entrada.nextInt();
+                    System.out.printf(" - Digite o denominador da 2º fração: ");
+                    int denominador_2 = entrada.nextInt();
+                    // caso os denominadores forem iguais
+                    int subtracao_fracao = numerador_1 - numerador_2;
+                    if (denominador_1 == denominador_2) {
+                        System.out.println(" >> O resultado é: "+subtracao_fracao+" / "+ denominador_1);
+                        System.out.printf(" > Fim do programa, deseja voltar ao menu principal? ");
+                        String escolha_7_2_1 = entrada.next();
+                        if (escolha_7_2_1.equals("sim")) {
+                            System.out.println(" \n Voltando ao menu principal...\n");
+                            break;
+                        }
+                        else {
+                            voltarMenu = false;
+                            System.out.println(" \n | PROGRAMA ENCERRADO! |\n");
+                        }
+                        break;
+                        // calculo de mmc
+                    } else {
+                        int sub_fracao = numerador_1 - numerador_2;
+                        int maior, mmc;
+                        if (denominador_1 > denominador_2) {
+                            maior = denominador_1;
+                        } else {
+                            maior = denominador_2;
+                        }
+                        while(true) {
+                            if ((maior % denominador_1 == 0) && (maior % denominador_2 == 0)) {
+                                mmc = maior;
+                                break;
+                            }
+                            maior++;
+                        }
+                        System.out.println(" >> O resultado é: "+sub_fracao+" / "+mmc);
+                        System.out.printf(" > fim do programa, deseja voltar ao menu principal? ");
+                        String voltar_7_2_2 = entrada.next();
+                        if (voltar_7_2_2.equals("sim")) {
+                            System.out.println(" \n Voltando ao menu principal...\n");
+                            break;
+                        }
+                        else {
+                            voltarMenu = false;
+                            System.out.println(" \n | PROGRAMA ENCERRADO! |\n");
+                        }
+                        break;
+                    }
+                }
+                // multiplicação de fração
+                if (escolha_7 == 3) {
+                    System.out.printf(" - Digite o numerador da 1º fração: ");
+                    int numerador_1 = entrada.nextInt();
+                    System.out.printf(" - Digite o denominador da 1º fração: ");
+                    int denominador_1 = entrada.nextInt();
+                    System.out.printf(" - Digite o numerador da 2º fração: ");
+                    int numerador_2 = entrada.nextInt();
+                    System.out.printf(" - Digite o denominador da 2º fração: ");
+                    int denominador_2 = entrada.nextInt();
+                    int multiplicacao_numeradores = numerador_1 * numerador_2;
+                    int multiplicacao_denominadores = denominador_1 * denominador_2;
+                    System.out.println(" >> O resultado é: "+multiplicacao_numeradores+ "/ "+multiplicacao_denominadores);
+                    System.out.printf(" > fim do programa, deseja voltar ao menu principal? ");
+                        String voltar_7_3 = entrada.next();
+                        if (voltar_7_3.equals("sim")) {
+                            System.out.println(" \n Voltando ao menu principal...\n");
+                            break;
+                        }
+                        else {
+                            voltarMenu = false;
+                            System.out.println(" \n | PROGRAMA ENCERRADO! |\n");
+                        }
+                        break;
+                }
+                // divisão de fração
+                if (escolha_7 == 4) {
+                    System.out.printf(" - Digite o numerador da 1º fração: ");
+                    int numerador_1 = entrada.nextInt();
+                    System.out.printf(" - Digite o denominador da 1º fração: ");
+                    int denominador_1 = entrada.nextInt();
+                    System.out.printf(" - Digite o numerador da 2º fração: ");
+                    int numerador_2 = entrada.nextInt();
+                    System.out.printf(" - Digite o denominador da 2º fração: ");
+                    int denominador_2 = entrada.nextInt();
+                    int multiplicacao_numeradores = numerador_1 * denominador_2;
+                    int multiplicacao_denominadores = numerador_2 * denominador_1;
+                    System.out.println(" >> O resultado é: "+multiplicacao_numeradores+ "/ "+multiplicacao_denominadores);
+                    System.out.printf(" > fim do programa, deseja voltar ao menu principal? ");
+                    String voltar_7_4 = entrada.next();
+                    if (voltar_7_4.equals("sim")) {
+                        System.out.println(" \n Voltando ao menu principal...\n");
+                        break;
+                    }
+                    else {
+                        voltarMenu = false;
+                        System.out.println(" \n | PROGRAMA ENCERRADO! |\n");
+                    }
+                    break;
+                }
+
+                case 8:
+                System.out.println("1. Bháskara \n2. Teorema de Pitágoras \n3. Área de um triângulo equilátero \n4. Função afim \n5. Função quadrática \n6. Área do círculo \n7. Volume do cilindro \n 8. Juros \n9. Função exponencial");
+                System.out.printf(" > Qual fórmula deseja usar? ");
+                int formula = entrada.nextInt();
+                // bhaskara
+                if (formula == 1) {
+                    
+                }
             }
         }
     }
